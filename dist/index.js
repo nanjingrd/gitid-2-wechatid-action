d6893921dfb8c3b0a4fcfe9e91ee6dca8e1e83ab
Datagate <datagate_service@example.com>
2022-08-24 15:06:47 +0800

    1.12

--- dist/index.js
+++ dist/index.js
@@ -9705,7 +9705,7 @@ try {
   const github_id = core.getInput('github_id');
   console.log(`Github id is ${github_id}!`);
   const time = (new Date()).toTimeString();
-  wecaht_id = realidmap[github_id]
+  var wecaht_id = realidmap[github_id]
   core.setOutput("wechat_id", wecaht_id);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
